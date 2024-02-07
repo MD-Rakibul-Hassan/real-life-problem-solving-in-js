e79e71279b4e346ce77fcc5a9d622b5d2a968b48
Rakibul <mdrakibulhassan2425@gmail.com>
2024-02-07 21:51:26 +0600

    Real life problem in js

---
+++ Problem/Problem.js
@@ -0,0 +1,58 @@
+function calculateMoney(ticketSale) {
+      if(ticketSale < 0) {
+            return "Invalud Number";
+      }
+      const ticketPrice = 120;
+      const daroanCost = 500;
+      const lanchCost = 8 * 50;
+      const totalTicketSale = ticketSale * ticketPrice;
+      const totalCost = daroanCost + lanchCost;
+      const totalEarning = totalTicketSale - totalCost;
+      return totalEarning;
+}
+
+
+function checkName(name) {
+      if (typeof name !== "string") {
+            return "invalid";
+      }
+      let nameLastLetter = name[name.length -1];
+      const goodNamesLastLetters = ["A","y","i","e","o","u","w","a","Y","I","E","O","U","W"];
+      if (goodNamesLastLetters.includes(nameLastLetter)) {
+            return "Good Name";
+      }else {
+         return "Bad Name";
+      }
+}
+
+
+function deleteInvalids(array) {
+      if (!Array.isArray(array)) {
+            return "Invalid Array";
+      }
+      const numberArray = array.filter( (value) => {
+            return (typeof value === "number" && !isNaN(value) );
+      })
+      return numberArray;
+}
+
+
+function password(obj) {
+      if(obj && !(obj.constructor.toString().indexOf("Object") > -1)) {
+            return "invalid"
+      }
+      if(obj.name && obj.birthYear && obj.siteName) {
+            const birthYearString = obj.birthYear.toString().length;
+            if (birthYearString === 4) {
+                  const siteNameFirstLetter = obj.siteName[0].toUpperCase();
+                  const siteNamelastLetters = obj.siteName.slice(1);
+                  const fullSiteName = siteNameFirstLetter + siteNamelastLetters;
+                  return `${fullSiteName}#${obj.name}@${obj.birthYear}`
+            }else {
+                  return "invalid" 
+            }
+      }else {
+            return "invalid" 
+      }
+}
+
